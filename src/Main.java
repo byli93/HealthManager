5674aa3bf7f8179d65f6f6eabdfbeb893dd90114
byli93 <bli079@aucklanduni.ac.nz>
2017-04-11 17:24:39 +1200

    layout of strucure

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,5 @@ public class Main {
 	//take input about activities and food consumption - logging function
 	//take input about period of evaluation
 	//calculate health rating of the specified period - rating function
+	
 }
