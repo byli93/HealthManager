78698f9e6bacf7827a7ba39c80d34f057c433466
byli93 <bli079@aucklanduni.ac.nz>
2017-04-11 19:54:07 +1200

    design layout of rating function

--- src/RatingFunction.java
+++ src/RatingFunction.java
@@ -1,4 +1,11 @@
 
 public class RatingFunction {
-//
+	//identify week of evaluation
+	// get info from log
+	// get rating from list
+	// do general calculation for rating
+	// if consume cheat meals more than once this week -18 points
+	// if went to gym at least 4 times +10, +2 extra for everytime after 4
+	
+	
 }
