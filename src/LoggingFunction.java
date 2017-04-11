69197716560e76e4e3925cc09bb314a7fa618421
byli93 <bli079@aucklanduni.ac.nz>
2017-04-11 19:42:17 +1200

    logging function layout

--- src/LoggingFunction.java
+++ src/LoggingFunction.java
@@ -1,4 +1,8 @@
 
 public class LoggingFunction {
-
+	//needs calendar
+	// get day for input
+	// get activity and food
+	// save to date
+	//stay in same screen after submit log, add button
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	//take input about activities and food consumption - logging function
-	//take input about period of evaluation
+	//all rating based per week, monthly rating is avg of 4 weeks
 	//calculate health rating of the specified period - rating function
 	
 }
