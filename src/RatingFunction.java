3a9f513300b1dd727635b7b23d8bfa180977e29c
byli93 <bli079@aucklanduni.ac.nz>
2017-04-19 16:30:14 +1200

    rating function complete ready for testing

--- src/RatingFunction.java
+++ src/RatingFunction.java
@@ -13,10 +13,14 @@ public class RatingFunction {
 	// do general calculation for rating
 	// if consume cheat meals more than once this week -18 points
 	// if went to gym at least 4 times +10, +2 extra ,each time after 4
-	public void rating(String[] dates){
+	public double rating(String[] dates){
 		List<Activity> actList = new ArrayList<Activity>(EnumSet.allOf(Activity.class));
 		String line;
 		double[] dailyscores = new double[dates.length];
+		double total = 0;
+		double finalscore = 50;
+		int cheatmeals = 0;
+		int gym = 0;
 		for(int i=0;i<dates.length;i++){
 			String path = "./" + dates[i]; 
 			Path file = Paths.get(path);
@@ -29,6 +33,9 @@ public class RatingFunction {
 				String[] intense = line.split(",");
 				for(int j=0;j<line.length();j++){
 					scores[j] = Integer.parseUnsignedInt(intense[j]) * actList.get(j).rating();
+					if(j == 21 && Integer.parseUnsignedInt(intense[j])<0){
+						cheatmeals += 1;
+					}
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -40,7 +47,20 @@ public class RatingFunction {
 			dailyscores[i] = dayscore;
 			dayscore = 0;
 		}
-		
+		for(int l =0;l<dailyscores.length;l++){
+			total += dailyscores[l];
+		}
+		finalscore = total/dailyscores.length;
+		if(cheatmeals < 1){
+			finalscore -= 18;
+		}
+		if(gym < 3){
+			finalscore += 10;
+			if(gym <4){
+				finalscore += (gym-4)*2;
+			}
+		}
+		return finalscore;
 	}
 
 	
